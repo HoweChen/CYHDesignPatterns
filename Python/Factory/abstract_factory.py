1a649d3842441dee77c700e1fb47e2a53654499d
HoweChen <yuhao.howe.chen@gmail.com>
2018-11-30 17:51:32 +0800

    update facotry method and abstract_factory

---
+++ Python/Factory/abstract_factory.py
@@ -0,0 +1,87 @@
+class Frog(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def interact_with(self, obstacle):
+        print(f"{self} the Frog encounters {obstacle} and {obstacle.action()}")
+
+
+class Bug(object):
+    def __str__(self):
+        return f"a bug"
+
+    def action(self):
+        return f"eats it"
+
+
+class FrogWorld(object):
+    def __init__(self, name):
+        print(self)
+        self.player_name = name
+
+    def __str__(self):
+        return f"\n\n\t------ Frog World ------"
+
+    def make_character(self):
+        return Frog(self.player_name)
+
+    def make_obstacle(self):
+        return Bug()
+
+
+class Wizard(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+    def interact_with(self, obstacle):
+        print(f"{self} the Wizard encounters {obstacle} and {obstacle.action()}")
+
+
+class Ork(object):
+    def __str__(self):
+        return f"an ork"
+
+    def action(self):
+        return f"beats it"
+
+
+class WizardWorld(object):
+    def __init__(self, name):
+        print(self)
+        self.player_name = name
+
+    def __str__(self):
+        return f"\n\n\t------ Wizard World ------"
+
+    def make_character(self):
+        return Wizard(self.player_name)
+
+    def make_obstacle(self):
+        return Ork()
+
+
+class GameEnvironment(object):
+    def __init__(self, factory):
+        self.hero = factory.make_character()
+        self.obstacle = factory.make_obstacle()
+
+    def play(self):
+        self.hero.interact_with(self.obstacle)
+
+
+def validate_age(age):
+    return False if age < 18 else True
+
+
+if __name__ == "__main__":
+    age = input("What is your age?")
+    name = input("What is your name?")
+    game = FrogWorld if not validate_age(age) else WizardWord
+    environment = GameEnvironment(game(name))
+    environment.play()
---
+++ Python/Factory/factory_method.py
@@ -0,0 +1,41 @@
+import xml.etree.ElementTree as etree
+import json
+
+
+class JSONConnector(object):
+    def __init__(self, filepath):
+        self._data = dict()
+        with open(filepath, mode=r, encoding="utf-8") as f:
+            self._data = json.load(f)
+
+    @property
+    def parsed_data(self):
+        return self._data
+
+
+class XMLConnecter(object):
+    def __init__(self, filepath):
+        self._tree = etree.parse(filepath)
+
+    @property
+    def parsed_data(self):
+        return self._tree
+
+
+def connection_factory(filepath):
+    if filepath.endswith("json"):
+        connector = JSONConnector
+    elif filepath.endswith("xml"):
+        connector = XMLConnecter
+    else:
+        raise ValueError(f"Cannot connect to {filepath}.")
+    return connector(filepath)
+
+
+def connect_to(filepath):
+    factory = None
+    try:
+        factory = connection_factory(filepath)
+    except ValueError as ve:
+        print(ve)
+    return factory
